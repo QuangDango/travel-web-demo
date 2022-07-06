45997ff39cfe3196e33469a165515f6504b15c5c
unknown <doanddquang98@gmail.com>
2022-07-06 22:54:09 +0700

    update source code

--- src/Components/MainContent/AvantureIdea.jsx
+++ src/Components/MainContent/AvantureIdea.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './Maincontent.css';
 
 const AvantureIdea = () => {
   return (
--- src/Components/MainContent/Banner.jsx
+++ src/Components/MainContent/Banner.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import 'Maincontent.css'
 import BtnControls from './BtnControls'
 
 
